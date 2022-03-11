69d82558fa3dbb0666f530228720d05da39bdaeb
nschloesserm <nicholasmckennaart@gmail.com>
2022-03-11 12:18:29 +0000

    add images

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,97 @@
-document.addEventListener("DOMContentLoaded", function() {
-    let buttons = document.getElementsByTagName("button");
+document.addEventListener("DOMContentLoaded", () => {
 
-    for (let button of buttons) {
-        button.addEventListener("click", function() {
-            if (this.getAttribute("data-type") === "submit") {
-                setInterval(timer, 1000);
-            } 
-        });
-    }
-});
-
-let count = 10;
-
-function timer () {
-  if(count > 0){
-    document.getElementById("timerContent").innerText = count;
-    count--;
-  } else if(count === 0){
-    document.getElementById("timerContent").innerText = "BOOM!!!";
+const codeArray = [
+  {
+    name: 'red',
+    img: '/assets/images/red.webp'
+  },
+  {
+    name: 'red',
+    img: '/assets/images/red.webp'
+  },
+  {
+    name: 'red',
+    img: '/assets/images/red.webp'
+  },
+  {
+    name: 'red',
+    img: '/assets/images/red.webp'
+  },
+  {
+    name: 'blue',
+    img: '/assets/images/blue.webp'
+  },
+  {
+    name: 'blue',
+    img: '/assets/images/blue.webp'
+  },
+  {
+    name: 'blue',
+    img: '/assets/images/blue.webp'
+  },
+  {
+    name: 'blue',
+    img: '/assets/images/blue.webp'
+  },
+  {
+    name: 'yellow',
+    img: '/assets/images/yellow.webp'
+  },
+  {
+    name: 'yellow',
+    img: '/assets/images/yellow.webp'
+  },
+  {
+    name: 'yellow',
+    img: '/assets/images/yellow.webp'
+  },
+  {
+    name: 'yellow',
+    img: '/assets/images/yellow.webp'
+  },
+  {
+    name: 'green',
+    img: '/assets/images/green.webp'
+  },
+  {
+    name: 'green',
+    img: '/assets/images/green.webp'
+  },
+  {
+    name: 'green',
+    img: '/assets/images/green.webp'
+  },
+  {
+    name: 'green',
+    img: '/assets/images/green.webp'
   }
-};
+]
+
+const grid = document.querySelector('.grid')
+var codeChosen =[]
+var codeChosenId = []
+
+function createBoard() {
+  for (let i = 0; i < codeArray.length; i++) {
+    var codeLight = document.createElement('img')
+    codeLight.setAttribute('src', '/assets/images/grey.webp')
+    codeLight.setAttribute('data-id, i')
+    codeLight.addEventListener('click', switchLight)
+    grid.appendChild(codeLight)
+  }
+}
+
+function switchLight() {
+  var codeLightId = this.getAttribute('data-id')
+  codeChosen.push(codeArray[codeLightId].name)
+  codeChosenId.push(cardId)
+  this.setAttribute('src', codeArray[codeLightId].img)
+  if (codeChosen.length === 4) {
+    setTimeout(checkForMatch, 500)
+  }
+}
+
+createBoard()
+
+})
 
